aefc18d04332040c5419c46009449b0b9aadd13f
ericsun1234567 <60624120+ericsun1234567@users.noreply.github.com>
2021-11-17 16:38:37 -0600

    Predicate 1 CACC Test

---
+++ ThermostatTest_CACC.java
@@ -0,0 +1,39 @@
+package Thermostat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ThermostatTest_CACC {
+
+	@Test
+	void ccacTruetestp1() {
+		ProgrammedSettings p = new ProgrammedSettings();
+		Thermostat t= new Thermostat();
+		t.setCurrentTemp(60);
+		t.setdTemp(70);
+		t.setThresholdDiff(5);
+		t.setOverride(false);
+		t.setOverTemp(75);
+		t.setTimeSinceLastRun(11);
+		t.setMinLag(10);
+		boolean result = t.turnHeaterOn();
+		assertTrue(result);
+	}
+	@Test
+	void ccacFalsetestp1() {
+		ProgrammedSettings p = new ProgrammedSettings();
+		Thermostat t= new Thermostat();
+		boolean result = t.turnHeaterOn();
+		t.setCurrentTemp(60);
+		t.setdTemp(70);
+		t.setThresholdDiff(5);
+		t.setOverride(false);
+		t.setOverTemp(60);
+		t.setTimeSinceLastRun(11);
+		t.setMinLag(10);
+		assertFalse(result);
+	}
+	
+
+}
